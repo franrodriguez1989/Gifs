380d60166e5b7b53da688a5b723e0114067b5b78
Francisco Rodriguez <pako_rh_89@hotmail.com>
2024-04-26 11:11:53 +0200

    Incluir formulario

--- src/App.js
+++ src/App.js
@@ -8,22 +8,11 @@ export default function App() {
     <div className="App">
       <section className="App-content">
       {<h1>Buscador de Gifs</h1>}
-      <Link href='http://localhost:3000/'>Ir al principio</Link>
-      <Link href='/gif/Vinicius'>Vinicius</Link>
-      <Link href='/gif/Bellingham'>Bellingham</Link> 
-      <Link href='/gif/Mbappe'>Mbappe</Link>
-      <span className="App">
-      <form /* onSubmit={handleSubmit} */>
-        <label htmlFor="keyword">Introduce un término de búsqueda:</label>
-        <input
-          type="text"
-          id="keyword"
-          /*value={keyword}
-          onChange={(event) => setKeyword(event.target.value)}
-          required */
-        />
-        <button type="submit">Buscar</button>
-      </form></span>
+      <Link to='http://localhost:3000'>Ir al principio</Link>
+      <Link to='/gif/Vinicius'>Vinicius</Link>
+      <Link to='/gif/Bellingham'>Bellingham</Link> 
+      <Link to='/gif/Mbappe'>Mbappe</Link>
+      
         <Route component={ListOfGifs} path='/gif/:keyword'/>
       {/* <ListOfGifs keyword={keyword} /> */}
       </section>
